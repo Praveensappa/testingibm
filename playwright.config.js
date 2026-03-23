a9af7790cd8d502ecd47fcc9d9e8e6759c4babc8
praveensappa <praveensappa2137@gmail.com>
2026-03-23 09:52:00 +0530

    login page and registration page test cases

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,11 @@
-// @ts-check
+[['html'],['allure-report']]// @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+// impor[['html'],['allure-report']] dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'],['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
